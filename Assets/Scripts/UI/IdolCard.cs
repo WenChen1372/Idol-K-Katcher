15af902c0a5200eb05b300a9c124b332f521a9d6
WenChen1372 <wen.biao.chen2002@gmail.co^[[>
2022-10-22 12:20:02 -0700

    added stuff

---
+++ Assets/Scripts/UI/IdolCard.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class IdolCard : MonoBehaviour
+{
+
+    public string Name;
+    public int cardID;
+    public bool selected;
+
+    
+}
---
+++ Assets/Scripts/UI/SelectIdolButton.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectIdolButton : MonoBehaviour
+{
+
+    //public int itemID;
+
+    //public void Select() {
+      //for (int i = 0; i < Inventory.inventory.cardList.Count; i ++ )
+      //{
+            //if (!Inventory.inventory.cardList[i].selected && Inventory.inventory.cardList[i].itemID == itemID)
+            //{
+                //then we want to do the select animation with the exchange.
+            //}
+      //}  
+    //}
+    // Start is called before the first frame update
+    //void Start()
+    //{
+        
+    //}
+
+    // Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+}
